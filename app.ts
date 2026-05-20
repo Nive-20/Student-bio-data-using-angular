dc923529e85d0f7c35f0f1e474bee3278eae677a
Nive-20 <144523671+Nive-20@users.noreply.github.com>
2026-05-20 10:11:56 +0530

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { StudentComponent } from './student.component/student.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [StudentComponent],
+  template: `<app-student></app-student>`,
+})
+export class App {
+}
